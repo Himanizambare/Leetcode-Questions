33818f72f62446eb549c999e67d034f9c2fa658c
Himani zambare <143264025+Himanizambare@users.noreply.github.com>
2025-09-20 12:56:14 +0530

    Time: 109 ms (34.99%) | Memory: 57.8 MB (28.02%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,50 @@
+
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        int n = heights.length;
+        int nsr[] = new int[n]; // next smaller right
+        int nsl[] = new int[n]; // next smaller left
+
+        // Next Smaller Right
+        Stack<Integer> s = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                nsr[i] = n;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Next Smaller Left
+        s = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Calculate areas
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        return maxArea;
+    }
+}
